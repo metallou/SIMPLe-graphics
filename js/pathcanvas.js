0740a68a460766afeabe593ade6616c256499062
Metallou <kevincastejon13@gmail.com>
2016-12-07 15:37:03 +0100

    finally resolved bug (objects are evil)

---
+++ js/pathcanvas.js
@@ -0,0 +1,49 @@
+let pathcanvas = function() {
+    let mouseX = window.innerWidth/2;
+    let mouseY = window.innerHeight/2;
+    let path = [];
+    let maxpathstops = 100;
+    let maxpathlength = 100;
+    let canv = document.getElementById("canv");
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight;
+    let ctx = canv.getContext("2d");
+
+    let updateMousePos = function() {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    }
+    document.addEventListener("mousemove", updateMousePos);
+
+    let updatePath = function() {
+        path.push({X: mouseX, Y: mouseY});
+        if(path.length > maxpathstops) {
+            path.shift();
+        }
+    }
+
+    let scrollPath = function() {
+        for(let i=0; i<path.length; i++) {
+            path[i].Y += 1;
+        }
+    }
+
+    let drawPath = function() {
+        ctx.clearRect(0,0, canv.width,canv.height);
+        ctx.beginPath();
+        ctx.moveTo(path[0].X, path[0].Y);
+        for(let i=1; i<path.length; i++) {
+            ctx.lineTo(path[i].X, path[i].Y);
+        }
+        ctx.stroke();
+    }
+
+    let funcpath = function() {
+        updatePath();
+        scrollPath();
+        drawPath();
+    }
+    setInterval(funcpath, 15);
+}
+
+document.addEventListener("DOMContentLoaded", pathcanvas);
