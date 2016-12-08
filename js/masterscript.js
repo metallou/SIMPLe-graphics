d6c76e16768dd86a12f70404ab7b48cd1fcef7dc
Metallou <kevincastejon13@gmail.com>
2016-12-08 12:20:53 +0100

    narrowed down variables definitions

--- js/masterscript.js
+++ js/masterscript.js
@@ -1,10 +1,45 @@
-let game = function() {
-    return "score";
+//Not my task (to be removed soon)
+let createNewLineBlocks = function() {};
+let deleteFinishedLineBlocks = function() {};
+
+let mastergamescript = function() {
+
+    let score = 0;
+    let isDead = false;
+    let nowDead = function() {
+        isDead = true;
+    }
+    let danger_blocks;
+    let intervalID;
+
+    //boucle de jeu
+    let gamefunc = function() {
+        if(!isDead) {
+            console.log("a");
+            createNewLineBlocks();
+
+            danger_blocks = document.getElementsByClassName("danger");
+            for(let i=0; i<danger_blocks.length; i++) {
+                danger_blocks[i].addEventListener("mouseover", nowDead);
+            }
+
+            if(Math.floor(Math.random()*1000*2)%16==0) {
+                nowDead();
+            }
+
+            score++;
+
+            deleteFinishedLineBlocks();
+            for(let i=0; i<danger_blocks.length; i++) {
+                danger_blocks[i].removeEventListener("mouseover", nowDead);
+            }
+        } else {
+            clearInterval(intervalID);
+            console.log("b");
+            console.log("Mettre le score dans le Local Storage: " + score);
+        }
+    }
+    intervalID = setInterval(gamefunc, 10);
 }
 
-let masterscript = function() {
-    let score = game();
-    console.log("mettre le score dans le Local Storage: " + score);
-}
-
-document.addEventListener("click", masterscript);
+document.addEventListener("click", mastergamescript);
