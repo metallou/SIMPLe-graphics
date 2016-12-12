1bad0cc2b996adaae71afd154c252d3e3c75592c
Metallou <kevincastejon13@gmail.com>
2016-12-12 22:56:08 +0100

    put function out of funcready

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,26 @@
-let buttonready = function()
+let blackScreen = function(time)
 {
-    let blackScreen = function()
+    let elem = document.createElement("div");
+    let blackScreenOff = function()
     {
-        let elem = document.createElement("div");
-        let blackScreenOff = function()
-        {
-            document.getElementById("wrapper").removeChild(elem);
-        }
-
-        elem.classList.add("blackscreen");
-        document.getElementById("wrapper").appendChild(elem);
-        setTimeout(blackScreenOff, 500);
+        document.getElementById("wrapper").removeChild(elem);
     }
 
+    elem.classList.add("blackscreen");
+    document.getElementById("wrapper").appendChild(elem);
+    setTimeout(blackScreenOff, time);
+}
+
+let buttonready = function()
+{
     document.getElementById("optionsbutton").addEventListener("click", function()
             {
-                blackScreen();
+                blackScreen(500);
                 document.getElementsByTagName("article")[0].style["left"] = "0px";
             });
     document.getElementById("statsbutton").addEventListener("click", function()
             {
-                blackScreen();
+                blackScreen(500);
                 document.getElementsByTagName("article")[0].style["left"] = "-200vw";
             });
     document.getElementById("playbutton").addEventListener("click", mastergamescript);
@@ -29,10 +29,9 @@ let buttonready = function()
     for(let i=0; i<buttons.length; i++) {
         buttons[i].addEventListener("click", function()
                 {
-                    blackScreen();
+                    blackScreen(500);
                     document.getElementsByTagName("article")[0].style["left"] = "-100vw";
                 });
     }
-
 }
 document.addEventListener("DOMContentLoaded", buttonready);
