dfcc6b55eb849057570a1a753e360d2b7879e9cc
Metallou <kevincastejon13@gmail.com>
2016-12-13 16:36:15 +0100

    fixed resetting localStorage

--- js/options.js
+++ js/options.js
@@ -1,67 +1,74 @@
-let optionsfuncready = function()
+let keysonoff =
+[
+    "music",
+    "sound"
+];
+let keys =
+[
+    "totalgamesplayed",
+    "totalscore",
+    "highestscore",
+    "averagescore",
+    "lastscore",
+    "totalbonusestaken",
+    "totalbosswavessurvived",
+    "totalfallingbosswavessurvived",
+    "totalrisingbosswavessurvived"
+];
+
+let optionsfunc = function()
 {
-
-    let optionsfunc = function()
+    let checkValue = function(key)
     {
-        let elem;
-        let elem2;
-        let keysonoff =
-            [
-            "music",
-            "sound"
-            ];
-
-        let checkvalue = function(key)
-        {
-            let elemon = document.getElementById(key+"on");
-            let elemoff = document.getElementById(key+"off");
-            if(localStorage.getItem(key) === "true") {
-                if(!elemon.classList.contains("selected")) {
-                    elemon.classList.add("selected");
-                    elemoff.classList.remove("selected");
-                }
-            } else {
-                if(!elemoff.classList.contains("selected")) {
-                    elemon.classList.remove("selected");
-                    elemoff.classList.add("selected");
-                }
+        let elemon = document.getElementById(key+"on");
+        let elemoff = document.getElementById(key+"off");
+        if(localStorage.getItem(key) === "true") {
+            if(!elemon.classList.contains("selected")) {
+                elemon.classList.add("selected");
+                elemoff.classList.remove("selected");
+            }
+        } else {
+            if(!elemoff.classList.contains("selected")) {
+                elemon.classList.remove("selected");
+                elemoff.classList.add("selected");
             }
         }
-
-        elem = document.getElementById("clearstorage").addEventListener("click", function()
-                {
-                    localStorage.clear();
-                    for(let i=0; i<keysonoff; i++) {
-                        localStorage.setItem(keysonoff[i], "true");
-                    }
-                    localStorage.setItem("totalgamesplayed", "0");
-                    localStorage.setItem("totalscore", "0");
-                    localStorage.setItem("highestscore", "0");
-                    localStorage.setItem("averagescore", "0");
-                    localStorage.setItem("totalbonusestaken", "0");
-                    localStorage.setItem("totalbosswavessurvived", "0");
-                    localStorage.setItem("totalfallingbosswavessurvived", "0");
-                    localStorage.setItem("totalrisingbosswavessurvived", "0");
-                    localStorage.setItem("lastscore", "0");
-                });
-
-        for(let i=0; i<keysonoff.length; i++) {
-            elem = document.getElementById(keysonoff[i]+"on");
-            elem2 = document.getElementById(keysonoff[i]+"off");
-            checkvalue(keysonoff[i]);
-
-            elem.addEventListener("click", function()
-                    {
-                        localStorage.setItem(keysonoff[i], "true");
-                        checkvalue(keysonoff[i]);
-                    });
-            elem2.addEventListener("click", function()
-                    {
-                        localStorage.setItem(keysonoff[i], "false");
-                        checkvalue(keysonoff[i]);
-                    });
-        }
     }
+
+    document.getElementById("clearstorage").addEventListener("click", function()
+            {
+                localStorage.clear();
+                for(let i=0; i<keysonoff.length; i++) {
+                    localStorage.setItem(keysonoff[i], "true");
+                    checkValue(keysonoff[i]);
+                }
+                for(let i=0; i<keys.length; i++) {
+                    localStorage.setItem(keys[i], 0);
+                }
+            });
+
+    let elem;
+    let elem2;
+    for(let i=0; i<keysonoff.length; i++) {
+        checkValue(keysonoff[i]);
+        elem = document.getElementById(keysonoff[i]+"on");
+        elem2 = document.getElementById(keysonoff[i]+"off");
+
+        elem.addEventListener("click", function()
+                {
+                    localStorage.setItem(keysonoff[i], "true");
+                    checkValue(keysonoff[i]);
+                });
+        elem2.addEventListener("click", function()
+                {
+                    localStorage.setItem(keysonoff[i], "false");
+                    checkValue(keysonoff[i]);
+                });
+    }
+}
+
+let optionsfuncready = function()
+{
     document.getElementById("optionsbutton").addEventListener("click", optionsfunc);
 }
 document.addEventListener("DOMContentLoaded", optionsfuncready);
--- js/stats.js
+++ js/stats.js
@@ -1,43 +1,21 @@
+let statsfunc = function()
+{
+    let nbelems = document.getElementsByTagName("tr").length;
+    let key;
+    let value;
+    for(let i=1; i<=nbelems; i++) {
+        key = document.getElementById("t"+i).innerHTML;
+        key = key.toLowerCase();
+        key = key.split("\n").join("");;
+        key = key.split(":").join("");
+        key = key.split(" ").join("");
+        value = localStorage.getItem(key);
+        document.getElementById("v"+i).innerHTML = value;
+    }
+}
+
 let statsfuncready = function()
 {
-
-    let statsfunc = function()
-    {
-        let nbelems = document.getElementsByTagName("tr").length;
-        let key;
-        let value;
-        for(let i=1; i<=nbelems; i++) {
-            key = document.getElementById("t"+i).innerHTML;
-            key = key.toLowerCase();
-            key = key.split("\n").join("");;
-            key = key.split(":").join("");
-            key = key.split(" ").join("");
-            value = localStorage.getItem(key);
-            document.getElementById("v"+i).innerHTML = value;
-        }
-    }
     document.getElementById("statsbutton").addEventListener("click", statsfunc);
-
-    let keys =
-        [
-        "totalgamesplayed",
-        "totalscore",
-        "highestscore",
-        "averagescore",
-        "totalbonusestaken",
-        "totalbosswavessurvived",
-        "totalfallingbosswavessurvived",
-        "totalrisingbosswavessurvived",
-        "lastscore"
-        ];
-    let val;
-    for(let i=0; i<keys.length; i++) {
-        val = localStorage.getItem(keys[i]);
-        if(val === null || val === undefined) {
-            localStorage.setItem(keys[i], "0");
-        }
-    }
-
-
 }
 document.addEventListener("DOMContentLoaded", statsfuncready);
