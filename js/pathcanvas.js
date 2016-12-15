c3ae6e31c8d0396133be690af128d61f6f68d48d
Eukavlin <brodin.elise@gmail.com>
2016-12-15 19:13:56 +0100

    Add smoke trail to space ship, not centered

--- js/pathcanvas.js
+++ js/pathcanvas.js
@@ -1,41 +1,45 @@
 //Global variable
 const path = [];
-const offsetDown = Math.floor(0.5*window.innerHeight/100);
+const offsetDown = Math.floor(0.5 * window.innerHeight / 100);
+const proutprout = new Image();
+proutprout.src = "img/prout.png";
+proutprout.height = '250%';
+proutprout.width = '250%';
 
-const funcPath = function()
-{
+
+const funcPath = function() {
     const canvas1 = document.getElementById("canvas1");
     canvas1.width = window.innerWidth;
     canvas1.height = window.innerHeight;
     const ctx1 = canvas1.getContext("2d");
-    ctx1.globalCompositeOperation="destination-over";
+    ctx1.globalCompositeOperation = "destination-over";
     const offX = 0;
-    const offY = document.getElementById("spaceship").offsetHeight*0.69;
+    const offY = document.getElementById("spaceship").offsetHeight * 0.69;
 
-    const updatePath = function(offsetX, offsetY)
-    {
-        const maxpathstops = 500;
+    const updatePath = function(offsetX, offsetY) {
+        const maxpathstops = 75;
 
-        path.push({X: mouseX+offsetX, Y: mouseY+offsetY});
-        if(path.length > maxpathstops) {
+        path.push({
+            X: mouseX + offsetX,
+            Y: mouseY + offsetY
+        });
+        if (path.length > maxpathstops) {
             path.shift();
         }
     }
-    const scrollPath = function()
-    {
-        for(let i=0; i<path.length; i++) {
+    const scrollPath = function() {
+        for (let i = 0; i < path.length; i++) {
             path[i].Y += offsetDown;
         }
     }
-    const drawPath = function()
-    {
-        ctx1.clearRect(0,0, canvas1.width,canvas1.height);
-        ctx1.beginPath();
+    const drawPath = function() {
+        ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
         ctx1.moveTo(path[0].X, path[0].Y);
-        for(let i=1; i<path.length; i++) {
+        for (let i = 1; i < path.length; i = i+3) {
+          ctx1.drawImage(proutprout, path[i].X, path[i].Y);
             ctx1.lineTo(path[i].X, path[i].Y);
         }
-        ctx1.stroke();
+
     }
 
     updatePath(offX, offY);
@@ -43,8 +47,7 @@ const funcPath = function()
     drawPath();
 }
 
-let funcPathReady = function()
-{
+let funcPathReady = function() {
     setInterval(funcPath, 10);
 }
 document.addEventListener("DOMContentLoaded", funcPathReady);
