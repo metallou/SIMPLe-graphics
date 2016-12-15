6db83d26d2af458efb1c881138d417c0c288bb90
Metallou <kevincastejon13@gmail.com>
2016-12-15 10:46:29 +0100

    simplified spaceship positionning

--- js/spaceship.js
+++ js/spaceship.js
@@ -1,11 +1,11 @@
 const updateSpaceshipReady = function() {
 
     const spaceship = document.getElementById("spaceship");
-    spaceship.style["left"] = (initMouseX - spaceship.offsetWidth/2) + "px";
+    spaceship.style["left"] = initMouseX + "px";
     spaceship.style["top"] = initMouseY + "px";
 
     let updateSpaceship = function() {
-        spaceship.style["left"] = (mouseX - spaceship.offsetWidth/2) + 'px',
+        spaceship.style["left"] = mouseX + 'px',
         spaceship.style["top"] =  (mouseY + 5) + 'px';
     }
     document.addEventListener("mousemove", updateSpaceship);
