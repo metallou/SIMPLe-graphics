836127441cdfdf5dea239e4cf88e113a4a2f32e6
Metallou <kevincastejon13@gmail.com>
2016-12-15 12:27:18 +0100

    random choice reenabled

--- js/masterscript.js
+++ js/masterscript.js
@@ -13,7 +13,7 @@ const mastergamescript = function()
     master_container.style["top"] = styletop + "px";
     document.getElementById("wrapper").appendChild(master_container);
     const levelChoice = (Math.floor(Math.random()*1000*100)%100);
-    const isBoss = levelChoice >= 0;
+    const isBoss = levelChoice >= 80;
     const isBossUp = isBoss && (levelChoice%2 == 0);
     const scoreBossUp = 10;
     const scoreBossDown = 20;
